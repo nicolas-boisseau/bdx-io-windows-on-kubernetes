6b14f08342836024087fd4b976e67f7d9008e411
nicolas.boisseau <nicolas.boisseau@outlook.com>
2025-09-18 09:09:27 +0200

    Refactor code structure for improved readability and maintainability

--- setup/mermaid.ts
+++ setup/mermaid.ts
@@ -6,10 +6,14 @@ import mermaid from 'mermaid/dist/mermaid.esm.mjs'
 // Choisis les packs d'icônes dont tu as besoin
 import { icons, icons as logos } from '@iconify-json/logos'
 import { icons as carbon } from '@iconify-json/carbon'
+import { icons as mdi } from '@iconify-json/mdi'
+
+// Enregistre les packs d'icônes
 
 mermaid.registerIconPacks([
   { name: logos.prefix, icons: logos },      // "logos"
   { name: carbon.prefix, icons: carbon },      // "carbon"
+  { name: mdi.prefix, icons: mdi },      // "mdi"
   { name: "azure", loader: () => fetch("/AzureIcons/icons.json").then(r => r.json()) } // prefix: azure
 ])
 
